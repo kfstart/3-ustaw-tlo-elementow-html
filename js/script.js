537c471947410f1adc9cffb9dacad180e380c190
kfstart <krystian.fik.kontakt@interia.pl>
2020-11-29 18:05:07 +0100

    html js

---
+++ js/script.js
@@ -0,0 +1,10 @@
+console.log('Warsztaty - ustaw tło elementów HTML');
+
+const setBackground = () => {
+    console.log('Click :)');
+}
+
+let btnSetBackground = document.getElementById('set-background');
+
+btnSetBackground.addEventListener('click', setBackground);
+
